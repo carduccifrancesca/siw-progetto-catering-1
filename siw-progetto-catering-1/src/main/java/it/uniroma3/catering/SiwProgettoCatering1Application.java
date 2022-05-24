970d89dec4b1b2562e7c5da1f1512bbfc02e8f79
carduccifrancesca <fra.carducci3@stud.uniroma3.it>
2022-05-24 14:49:53 +0200

    Inizializzazione

---
+++ siw-progetto-catering-1/src/main/java/it/uniroma3/catering/SiwProgettoCatering1Application.java
@@ -0,0 +1,13 @@
+package it.uniroma3.catering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwProgettoCatering1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwProgettoCatering1Application.class, args);
+	}
+
+}
